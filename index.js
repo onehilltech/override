4ebf41bdb8aec0979e99c3c41dd307b9b4e31d9e
James Hill <james@onehilltech.com>
2022-01-23 11:00:19 -0500

    feat: added override support fro async functions

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ function override (target, name, newFunc) {
 
   target[name] = function () {
     // Replace the current _super method with the one for this message.
-    let _super = this._super;
+    const _super = this._super;
     this._super = orig;
 
     // Call the overridden method.
-    newFunc.call (this, ...arguments);
+    const retval = newFunc.call (this, ...arguments);
 
     // Restore the original super method.
     this._super = _super;
+
+    return retval;
+  }
+}
+
+override.async = function override__Async (target, name, newFunc) {
+  const orig = target[name];
+
+  target[name] = async function () {
+    // Replace the current _super method with the one for this message.
+    const _super = this._super;
+    this._super = orig;
+
+    // Call the overridden method.
+    const retval = await newFunc.call (this, ...arguments);
+
+    // Restore the original super method.
+    this._super = _super;
+
+    return retval;
   }
 }
 
